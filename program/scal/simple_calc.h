9f86d7caf109dddcae92c16aabe74273cf243fcf
dtenpf <dtenpf@gmail.com>
2017-07-25 22:07:28 +0000

    update

--- program/scal/simple_calc.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SIMPLE_CALC_H__
-#define __SIMPLE_CALC_H__
-
-int scal_add(int a, int b);
-int scal_sub(int a, int b); 
-int scal_mul(int a, int b); 
-int scal_dev(int a, int b);
-
-#endif
--- program/smath/simple_math.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SIMPLE_MATH_H__
-#define __SIMPLE_MATH_H__
-
-int smath_pow(int a, int b);
-
-#endif
